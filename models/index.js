8c640430b16be300ebae45bb4c20bacc2e76e358
Troy Johnson <tnj8510@gmail.com>
2023-02-03 21:49:34 -0800

    add model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,25 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: 'productTag',
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product,{
+  through: 'productTag',
+})
+
 
 module.exports = {
   Product,
